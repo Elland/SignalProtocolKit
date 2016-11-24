70788847923a07b30ee9740ec76b85e6abfba4cc
Igor Ranieri Elland <igor@elland.me>
2016-11-24 15:56:16 +0100

    Updated umbrella header.

--- AxolotlKitiOS/AxolotlKitiOS.h
+++ AxolotlKitiOS/AxolotlKitiOS.h
@@ -16,4 +16,38 @@ FOUNDATION_EXPORT const unsigned char AxolotlKitiOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <AxolotlKitiOS/PublicHeader.h>
 
-
+#import <AxolotlKitiOS/AxolotlKitiOS.h>
+#import <AxolotlKit/Classes/AxolotlExceptions.h>
+#import <AxolotlKit/Classes/CipherMessage/CipherMessage.h>
+#import <AxolotlKit/Classes/CipherMessage/PreKeyWhisperMessage.h>
+#import <AxolotlKit/Classes/CipherMessage/WhisperMessage.h>
+#import <AxolotlKit/Classes/Constants.h>
+#import <AxolotlKit/Classes/Crypto/AES-CBC.h>
+#import <AxolotlKit/Classes/Prekeys/PreKeyBundle.h>
+#import <AxolotlKit/Classes/Prekeys/PreKeyRecord.h>
+#import <AxolotlKit/Classes/Prekeys/SignedPrekeyRecord.h>
+#import <AxolotlKit/Classes/Protobuffs/WhisperTextProtocol.pb.h>
+#import <AxolotlKit/Classes/Ratchet/AliceAxolotlParameters.h>
+#import <AxolotlKit/Classes/Ratchet/AxolotlParameters.h>
+#import <AxolotlKit/Classes/Ratchet/BobAxolotlParameters.h>
+#import <AxolotlKit/Classes/Ratchet/Chain.h>
+#import <AxolotlKit/Classes/Ratchet/ChainAndIndex.h>
+#import <AxolotlKit/Classes/Ratchet/ChainKey.h>
+#import <AxolotlKit/Classes/Ratchet/MessageKeys.h>
+#import <AxolotlKit/Classes/Ratchet/RatchetingSession.h>
+#import <AxolotlKit/Classes/Ratchet/ReceivingChain.h>
+#import <AxolotlKit/Classes/Ratchet/RKCK.h>
+#import <AxolotlKit/Classes/Ratchet/RootKey.h>
+#import <AxolotlKit/Classes/Ratchet/SendingChain.h>
+#import <AxolotlKit/Classes/Ratchet/TSDerivedSecrets.h>
+#import <AxolotlKit/Classes/SessionCipher.h>
+#import <AxolotlKit/Classes/Sessions/SessionBuilder.h>
+#import <AxolotlKit/Classes/Sessions/SessionRecord.h>
+#import <AxolotlKit/Classes/Sessions/SessionState.h>
+#import <AxolotlKit/Classes/State/AxolotlStore.h>
+#import <AxolotlKit/Classes/State/IdentityKeyStore.h>
+#import <AxolotlKit/Classes/State/PreKeyStore.h>
+#import <AxolotlKit/Classes/State/SessionStore.h>
+#import <AxolotlKit/Classes/State/SignedPreKeyStore.h>
+#import <AxolotlKit/Classes/Utility/NSData+keyVersionByte.h>
+#import <AxolotlKit/Classes/Utility/SerializationUtilities.h>
